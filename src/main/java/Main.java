8a88c557a6de4f90c2ccbac3f5566bf5bd63f56f
verbatium <orhdposit@gmail.com>
2017-07-02 13:59:41 +0300

    close all EntityManager instances in final block

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,59 +17,76 @@ import java.sql.SQLException;
 public class Main {
 
     private static EntityManagerFactory entityManagerFactory;
-    private static EntityManager entityManager;
 
     public static void main(String... args) throws IOException, LiquibaseException, SQLException {
         entityManagerFactory = Persistence.createEntityManagerFactory("MysqlPersistenceUnit");
-        updateDB();
-        addUser();
-        findUSer();
-        addUser();
-        entityManagerFactory.close();
+        try {
+            updateDB();
+            addUser();
+            findUser();
+            addUser();
+        }
+        finally {
+            entityManagerFactory.close();
+        }
     }
 
-    private static void findUSer() {
+    private static void findUser() {
+        EntityManager entityManager;
         entityManager = entityManagerFactory.createEntityManager();
-        Cache cache = entityManagerFactory.getCache();
-        boolean exists = cache.contains(User.class, "admin");
-        if(exists){
-            System.out.println("in cache");
-        }
-        else
-        {
-            System.out.println("not in cache");
+        try {
+            Cache cache = entityManagerFactory.getCache();
+            boolean exists = cache.contains(User.class, "admin");
+            if(exists){
+                System.out.println("in cache");
+            }
+            else
+            {
+                System.out.println("not in cache");
 
+            }
+            User admin = entityManager.find(User.class, "admin");
+            System.out.println(admin);
         }
-
-        entityManager.find(User.class,"admin");
-        entityManager.close();
+        finally {
+            entityManager.close();
+        }
     }
 
     private static void addUser() {
+        EntityManager entityManager;
         entityManager = entityManagerFactory.createEntityManager();
-        EntityTransaction userTransaction = entityManager.getTransaction();
-        userTransaction.begin();
-        User instance = new User();
-        instance.setUserName("admin");
-        instance.setPassword("password");
-        entityManager.merge(instance);
-        userTransaction.commit();
-        entityManager.close();
+        try {
+            EntityTransaction userTransaction = entityManager.getTransaction();
+            userTransaction.begin();
+            User instance = new User();
+            instance.setUserName("admin");
+            instance.setPassword("password");
+            entityManager.merge(instance);
+            userTransaction.commit();
+        }
+        finally {
+            entityManager.close();
+        }
     }
 
 
     private static void updateDB() throws LiquibaseException, SQLException {
+        EntityManager entityManager;
         entityManager = entityManagerFactory.createEntityManager();
+        try {
+            Session session = (Session) entityManager.getDelegate();
+            SessionFactoryImplementor sfi = (SessionFactoryImplementor) session.getSessionFactory();
+            Connection connection = sfi.getJdbcServices()
+                    .getBootstrapJdbcConnectionAccess()
+                    .obtainConnection();
 
-        Session session = (Session) entityManager.getDelegate();
-        SessionFactoryImplementor sfi = (SessionFactoryImplementor) session.getSessionFactory();
-        Connection connection = sfi.getJdbcServices()
-                .getBootstrapJdbcConnectionAccess()
-                .obtainConnection();
-
-        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
-        Liquibase liquibase = new liquibase.Liquibase("liquibase/migrate.xml", new ClassLoaderResourceAccessor(), database);
-        liquibase.update(new Contexts(), new LabelExpression());
-        entityManager.close();
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase = new liquibase.Liquibase("liquibase/migrate.xml", new ClassLoaderResourceAccessor(), database);
+            liquibase.update(new Contexts(), new LabelExpression());
+        }
+        finally {
+            entityManager.close();
+        }
     }
 }
