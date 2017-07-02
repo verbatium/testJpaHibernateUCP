0c06ebd1b01a0cf98276432bb47424e8f87d3648
verbatium <orhdposit@gmail.com>
2017-07-02 14:47:44 +0300

    add jetty server with jmx support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,11 +6,15 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.log.Log;
 import org.hibernate.Session;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.persistence.*;
-import java.io.IOException;
+import java.lang.management.ManagementFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -18,17 +22,32 @@ public class Main {
 
     private static EntityManagerFactory entityManagerFactory;
 
-    public static void main(String... args) throws IOException, LiquibaseException, SQLException {
+    public static void main(String... args) throws Exception {
         entityManagerFactory = Persistence.createEntityManagerFactory("MysqlPersistenceUnit");
         try {
             updateDB();
             addUser();
             findUser();
             addUser();
-        }
-        finally {
+            startServer();
+        } finally {
             entityManagerFactory.close();
         }
+    }
+
+    private static void startServer() throws Exception {
+        Server server = new Server(8080);
+        // Setup JMX
+        MBeanContainer mbContainer = new MBeanContainer(
+                ManagementFactory.getPlatformMBeanServer());
+        server.addBean(mbContainer);
+
+        // Export the loggers as MBeans.
+        server.addBean(Log.getLog());
+
+        server.start();
+        server.dumpStdErr();
+        server.join();
     }
 
     private static void findUser() {
@@ -37,18 +56,15 @@ public class Main {
         try {
             Cache cache = entityManagerFactory.getCache();
             boolean exists = cache.contains(User.class, "admin");
-            if(exists){
+            if (exists) {
                 System.out.println("in cache");
-            }
-            else
-            {
+            } else {
                 System.out.println("not in cache");
 
             }
             User admin = entityManager.find(User.class, "admin");
             System.out.println(admin);
-        }
-        finally {
+        } finally {
             entityManager.close();
         }
     }
@@ -61,11 +77,11 @@ public class Main {
             userTransaction.begin();
             User instance = new User();
             instance.setUserName("admin");
-            instance.setPassword("password");
+            String hashed = BCrypt.hashpw("password", BCrypt.gensalt(12));
+            instance.setPassword(hashed);
             entityManager.merge(instance);
             userTransaction.commit();
-        }
-        finally {
+        } finally {
             entityManager.close();
         }
     }
@@ -84,8 +100,7 @@ public class Main {
             Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
             Liquibase liquibase = new liquibase.Liquibase("liquibase/migrate.xml", new ClassLoaderResourceAccessor(), database);
             liquibase.update(new Contexts(), new LabelExpression());
-        }
-        finally {
+        } finally {
             entityManager.close();
         }
     }
